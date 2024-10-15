01f0929d02f8dc80416a1a5bef2304ffb7fce99d
luckyanand15 <luckyanand.741@gmail.com>
2024-10-15 20:31:28 +0530

    X-DisplayName label corrected

--- xdisplayname/src/App.js
+++ xdisplayname/src/App.js
@@ -7,32 +7,28 @@ function App() {
   const [isClicked, setIsClicked] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
-    setFullName(fname +" "+ lname);
+    setFullName(fname + " " + lname);
     setIsClicked(true);
   };
   return (
     <div>
       <h1>Full Name Display</h1>
-      <form onSubmit={(e)=>handleSubmit(e)}>
-        <label>
-          First Name:
-          <input
-            type="text"
-            value={fname}
-            onChange={(e) => setFname(e.target.value)}
-            required
-          />
-        </label>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>First Name:</label>
+        <input
+          type="text"
+          value={fname}
+          onChange={(e) => setFname(e.target.value)}
+          required
+        />
         <br />
-        <label>
-          Last Name:
-          <input
-            type="text"
-            value={lname}
-            onChange={(e) => setLname(e.target.value)}
-            required
-          />
-        </label>
+        <label>Last Name:</label>
+        <input
+          type="text"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
+          required
+        />
         <br />
         <button>Submit</button>
         {isClicked && <p>Full Name: {fullName}</p>}
